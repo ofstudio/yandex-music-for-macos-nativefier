03319ec046c700fefbb4f60267e41d0fc28473b4
Oleg Fomin <ofstudio@gmail.com>
2021-01-08 15:09:08 +0300

    add back button

--- src/inject.js
+++ src/inject.js
@@ -2,3 +2,16 @@
 const draggableBar = document.createElement('div')
 draggableBar.id = 'macos-draggable-bar'
 document.body.appendChild(draggableBar)
+
+
+// add back button
+const headerContainer = document.getElementsByClassName('head__left')[0]
+if (headerContainer !== undefined) {
+  const backButtonContainer = document.createElement('span')
+  backButtonContainer.id = 'back-button-container'
+  const backButton = document.createElement('a')
+  backButton.id = 'back-button'
+  backButtonContainer.appendChild(backButton)
+  headerContainer.insertBefore(backButtonContainer, headerContainer.childNodes[0])
+  backButton.addEventListener('click', () => window.history.back())
+}
